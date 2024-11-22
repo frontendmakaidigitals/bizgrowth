237bfba2bc6ad1de96adb3f02de850fdd420a5b1
Md Faheem <90985169+faheem221@users.noreply.github.com>
2024-11-22 16:53:38 +0530

    added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,7 +13,27 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        Satoshi: ["Satoshi , sans-serif"],
+        Synonym: ["Synonym , sans-serif"],
+        SplineSans: ["Spline Sans , sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          sm: "100%",
+          md: "768px",
+          lg: "1024px",
+          xl: "1380px",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
