2e761a0c415f157fdc27779e0c6f6a35c2cce857
Md Faheem <90985169+faheem221@users.noreply.github.com>
2024-11-22 16:59:51 +0530

    done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,39 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ["class"],
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        Satoshi: ["Satoshi , sans-serif"],
-        Synonym: ["Synonym , sans-serif"],
-        SplineSans: ["Spline Sans , sans-serif"],
-      },
-      container: {
-        center: true,
-        padding: "2rem",
-        screens: {
-          sm: "100%",
-          md: "768px",
-          lg: "1024px",
-          xl: "1380px",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		fontFamily: {
+  			Satoshi: ["Satoshi , sans-serif"],
+  			Synonym: ["Synonym , sans-serif"],
+  			SplineSans: ["Spline Sans , sans-serif"]
+  		},
+  		container: {
+  			center: true,
+  			padding: '2rem',
+  			screens: {
+  				sm: '100%',
+  				md: '768px',
+  				lg: '1024px',
+  				xl: '1380px'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 } satisfies Config;
