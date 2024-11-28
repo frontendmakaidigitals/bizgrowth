864d94d943a3b44d21f0177ab46b71b5708e3b0c
Faheem <Faheemk793@gmail.com>
2024-11-28 11:32:37 +0530

    added select

--- app/App_Chunks/Components/Form.tsx
+++ app/App_Chunks/Components/Form.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import Heading from "./Heading";
 import Button from "./Button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectGroup,
+  SelectLabel,
+} from "@/components/ui/select";
 const Form = () => {
   return (
     <div className="w-full py-14 bg-yellow-100">
@@ -19,22 +28,36 @@ const Form = () => {
               <label className="font-[500] text-xl ">Full Name</label>
               <input
                 placeholder="Enter your full name"
-                className="bg-slate-950/10 rounded-lg focus:shadow-md focus:outline-dashed  px-4 py-2 mt-1"
+                className="bg-slate-950/10 placeholder:text-slate-600 rounded-lg focus:shadow-md focus:outline-dashed  px-4 py-2 mt-1"
               />
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-4">
               <div className="grid grid-cols-1">
                 <label className="font-[500] text-xl ">Company</label>
-                <input
-                  placeholder="Enter your company name"
-                  className="bg-slate-950/10 rounded-lg focus:shadow-md focus:outline-dashed  px-4 py-2 mt-1"
-                />
+                <div>
+                  <Select>
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select a fruit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Fruits</SelectLabel>
+                        <SelectItem value="apple">Apple</SelectItem>
+                        <SelectItem value="banana">Banana</SelectItem>
+                        <SelectItem value="blueberry">Blueberry</SelectItem>
+                        <SelectItem value="grapes">Grapes</SelectItem>
+                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
               </div>
               <div className="grid grid-cols-1 ">
                 <label className="font-[500] text-xl ">Contact</label>
                 <input
+                  type="number"
                   placeholder="Enter Contact Number"
-                  className="bg-slate-950/10 rounded-lg focus:shadow-md focus:outline-dashed  px-4 py-2 mt-1"
+                  className="bg-slate-950/10 placeholder:text-slate-600 rounded-lg focus:shadow-md focus:outline-dashed  px-4 py-2 mt-1"
                 />
               </div>
             </div>
@@ -42,14 +65,14 @@ const Form = () => {
               <label className="font-[500] text-xl ">Email</label>
               <input
                 placeholder="Eter your email"
-                className="bg-slate-950/10 rounded-lg focus:shadow-md focus:outline-dashed  px-4 py-2 mt-1"
+                className="bg-slate-950/10 placeholder:text-slate-600 rounded-lg focus:shadow-md focus:outline-dashed  px-4 py-2 mt-1"
               />
             </div>
             <div className="grid grid-cols-1 mt-4">
               <label className="font-[500] text-xl ">Message</label>
               <textarea
                 placeholder="Enter your message"
-                className="resize-none bg-slate-950/10 rounded-lg focus:shadow-md focus:outline-dashed  h-28 px-4 py-2 mt-1"
+                className="resize-none bg-slate-950/10 placeholder:text-slate-600 rounded-lg focus:shadow-md focus:outline-dashed  h-28 px-4 py-2 mt-1"
               />
             </div>
             <Button className="px-4 mt-4 py-2 bg-blue-300 hover:bg-blue-400 rounded-lg ">
