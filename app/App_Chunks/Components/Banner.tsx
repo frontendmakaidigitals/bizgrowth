9cd181ddce5132f4714d9f8d96af95267bfa94ee
Faheem <Faheemk793@gmail.com>
2024-11-29 19:32:00 +0530

    done

--- app/App_Chunks/Components/Banner.tsx
+++ app/App_Chunks/Components/Banner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "./Button";
 const Banner = ({ query }: { query: string | undefined | null }) => {
   return (
-    <div className="w-full container mb-24 rounded-xl p-7 bg-[url('/media/gggrain.svg')]">
+    <div className="w-full container border border-slate-300 mb-24 rounded-xl p-7 bg-[url('/media/gggrain.svg')]">
       <div>
         <p className="font-Synonym font-[600] text-lime-950 text-2xl tracking-tight">
           READY TO KICKSTART YOUR BUSINESS IN {query}?
