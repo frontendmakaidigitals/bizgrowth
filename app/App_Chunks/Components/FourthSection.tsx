40509882b11dc5312bc999110a9e3a9efc29bcb6
Faheem <Faheemk793@gmail.com>
2024-12-17 14:11:15 +0530

    removed animate presence

--- app/App_Chunks/Components/FourthSection.tsx
+++ app/App_Chunks/Components/FourthSection.tsx
@@ -7,7 +7,7 @@ import { FaPassport } from "react-icons/fa";
 import { MdOutlineAccountBalance } from "react-icons/md";
 import { FaUserTie } from "react-icons/fa";
 import { PiOfficeChairBold } from "react-icons/pi";
-import { AnimatePresence, motion } from "framer-motion";
+import {  motion } from "framer-motion";
 import { HiOutlineTableCells } from "react-icons/hi2";
 import { GoSponsorTiers } from "react-icons/go";
 import { MdAssistant } from "react-icons/md";
