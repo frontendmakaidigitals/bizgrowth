91cd9565c205f54bfc2438d005e3a9811a9c7815
frontendmakaidigitals <frontendmakaidigitals@gmail.com>
2025-02-08 10:51:50 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
- 
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./App_Chunks/Components/Navbar";
@@ -6,6 +5,8 @@ import Footer from "./App_Chunks/Components/Footer";
 import { Providers } from "./App_Chunks/Components/ParallaxProvider";
 import Loading from "./App_Chunks/Components/Loading";
 import FloatingWhatsIcon from "./App_Chunks/Components/floatingWhatsIcon";
+import Head from "next/head"; // Import next/head
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -16,7 +17,6 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
- 
 
 export default function RootLayout({
   children,
@@ -26,35 +26,52 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '590278050828322');
-    fbq('track', 'PageView');
-  </script>
-  <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=590278050828322&ev=PageView&noscript=1"
-  /></noscript>
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=satoshi@300,400,500,700,900&f[]=synonym@200,300,400,500,600,700&f[]=spline-sans@300,400,500,600,700&display=swap"
-          rel="stylesheet"
-        ></link>
+        <Head>
+          {/* Meta Pixel Code */}
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s) {
+                  if(f.fbq)return;
+                  n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;
+                  n.push=n;
+                  n.loaded=!0;
+                  n.version='2.0';
+                  n.queue=[];
+                  t=b.createElement(e);
+                  t.async=!0;
+                  t.src=v;
+                  s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)
+                }(window, document,'script', 'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '590278050828322');
+                fbq('track', 'PageView');
+              `
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=590278050828322&ev=PageView&noscript=1"
+            />
+          </noscript>
+          <link
+            href="https://api.fontshare.com/v2/css?f[]=satoshi@300,400,500,700,900&f[]=synonym@200,300,400,500,600,700&f[]=spline-sans@300,400,500,600,700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         <Loading />
         <FloatingWhatsIcon />
         <Providers>{children}</Providers>
         <Footer />
-
       </body>
     </html>
   );
