caaffcea171a48be6a12e564e3dfd3ea1d92f042
faheem221 <faheemk793@gmail.com>
2026-07-14 01:42:55 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   trailingSlash: false,
+  serverExternalPackages: [
+    "@prisma/client",
+    "app/generated/prisma",
+  ],
 };
 
 export default nextConfig;
