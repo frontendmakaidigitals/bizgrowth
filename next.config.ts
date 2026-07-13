e465da6e6001f401329264feadb1d839b5acc11f
faheem221 <faheemk793@gmail.com>
2026-07-14 02:03:43 +0530

    fix: use default Prisma client location to resolve engine path bug on deploy

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   trailingSlash: false,
-  serverExternalPackages: [
-    "@prisma/client",
-    "app/generated/prisma",
-  ],
 };
 
 export default nextConfig;
